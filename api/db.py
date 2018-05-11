dd3190d5890ed26c15b6275e4aed3c4fe1a8d00a
mikewynn2 <mikewynn2@mac.com>
2018-05-10 18:17:41 -0700

    API is almost done
    -DB model is complete;
    -Controller is almost complete;

--- api/db.py
+++ api/db.py
@@ -3,6 +3,10 @@ from exceptions import UserExists
 import datetime
 import os.path
 
+USER_COLUMNS = ['id', 'email', 'username', 'first_name', 'last_name', 'date_created']
+POST_COLUMNS = ['id', 'author_id', 'title', 'content', 'date_created', 'date_edited']
+COMMENT_COLUMNS = ['id', 'author_id', 'post_id', 'content', 'date_created', 'date_edited']
+
 
 class DBEngine(object):
     def __init__(self):
@@ -33,6 +37,149 @@ class DBEngine(object):
             return c.lastrowid
         except sqlite3.IntegrityError:
             raise UserExists()
+
+    def create_post(self, author, title, content):
+        try:
+            c = self.conn.cursor()
+            c.execute(
+                """
+                INSERT INTO post (author_id, title, content, date_created, date_edited)
+                          VALUES (?, ?, ?, ?, ?)
+                """, [
+                    author,
+                    title,
+                    content,
+                    datetime.datetime.utcnow().timestamp(),
+                    datetime.datetime.utcnow().timestamp()
+                ]
+            )
+            self.conn.commit()
+            return c.lastrowid
+        except Exception as e:
+            print(e)
+
+    def create_comment(self, author, post_id, content, response=None):
+        try:
+            c = self.conn.cursor()
+            if response is None:
+                c.execute(
+                    """
+                    INSERT INTO comment (author_id, post_id, content, date_created, date_edited)
+                              VALUES (?, ?, ?, ?, ?)
+                    """, [
+                        author,
+                        post_id,
+                        content,
+                        datetime.datetime.utcnow().timestamp(),
+                        datetime.datetime.utcnow().timestamp(),
+                    ]
+                )
+            else:
+                c.execute(
+                    """
+                    INSERT INTO comment (author_id, post_id, response, content, date_created, date_edited)
+                              VALUES (?, ?, ?, ?, ?, ?)
+                    """, [
+                        author,
+                        post_id,
+                        response,
+                        content,
+                        datetime.datetime.utcnow().timestamp(),
+                        datetime.datetime.utcnow().timestamp(),
+                    ]
+                )
+            self.conn.commit()
+            return c.lastrowid
+        except Exception as e:
+            print(e)
+
+    def get_users(self):
+        try:
+            c = self.conn.cursor()
+            users = c.execute(
+                """SELECT * FROM user ORDER BY id;"""
+            ).fetchall()
+            return [process_record(USER_COLUMNS, user) for user in users]
+
+        except Exception as e:
+            print(e)
+
+    def get_posts(self):
+        try:
+            c = self.conn.cursor()
+            posts = c.execute(
+                """SELECT * FROM post ORDER BY id;"""
+            ).fetchall()
+            return [process_record(POST_COLUMNS, post) for post in posts]
+
+        except Exception as e:
+                print(e)
+
+    def get_comments(self, post_id):
+        try:
+            c = self.conn.cursor()
+            comments = c.execute(
+                """SELECT * FROM comment WHERE post_id = ? ORDER BY id;""", [post_id]
+            ).fetchall()
+            return [process_record(COMMENT_COLUMNS, comment) for comment in comments]
+
+        except Exception as e:
+            print(e)
+
+    def get_user(self, user_id):
+        try:
+            c = self.conn.cursor()
+            user = c.execute("""SELECT * FROM user WHERE id = ?;""", [user_id]).fetchone()
+
+            return process_record(USER_COLUMNS, user)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def get_post(self, post_id):
+        try:
+            c = self.conn.cursor()
+            post = c.execute("""SELECT * FROM post WHERE id = ?;""", [post_id]).fetchone()
+
+            return process_record(POST_COLUMNS, post)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def get_comment(self, comment_id):
+        try:
+            c = self.conn.cursor()
+            comment = c.execute("""SELECT * FROM comment WHERE id = ?;""", [comment_id]).fetchone()
+
+            return process_record(COMMENT_COLUMNS, comment)
+        except Exception as e:
+            print(e)
+            raise e
+
+    def delete_user(self, user_id):
+        try:
+            c = self.conn.cursor()
+            c.execute("""DELETE FROM user WHERE id = ?;""", [user_id])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    def delete_post(self, post_id):
+        try:
+            c = self.conn.cursor()
+            c.execute("""DELETE FROM post WHERE id = ?;""", [post_id])
+            c.execute("""DELETE FROM comment WHERE post_id = ?;""", [post_id])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    def delete_comment(self, comment_id):
+        try:
+            c = self.conn.cursor()
+            c.execute("""DELETE FROM comment WHERE id = ?;""", [comment_id])
+            self.conn.commit()
+        except Exception as e:
+            print(e)
 
 
 def init_db(db_name=None):
@@ -72,11 +219,13 @@ def init_db(db_name=None):
             id INTEGER PRIMARY KEY,
             author_id INTEGER NOT NULL,
             post_id INTEGER NOT NULL,
+            response INTEGER,
             content TEXT NOT NULL,
             date_created INTEGER NOT NULL,
             date_edited INTEGER,
             FOREIGN KEY (author_id) REFERENCES user(id),
-            FOREIGN KEY (post_id) REFERENCES post(id)
+            FOREIGN KEY (post_id) REFERENCES post(id),
+            FOREIGN KEY (response) REFERENCES comment(id)
         )""")
 
         conn.commit()
@@ -87,6 +236,13 @@ def init_db(db_name=None):
         result = False
 
     return result
+
+
+def process_record(column_names, record):
+    processed_record = {}
+    for i, column in enumerate(column_names):
+        processed_record[column] = record[i]
+    return processed_record
 
 
 if __name__ == '__main__':
--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, redirect
+from db import DBEngine
 
 
 app = Flask(__name__)
@@ -7,7 +8,75 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'hello': 'World'}), 200
+    return redirect('/user')
+
+
+@app.route('/user/<int:user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    return jsonify(DBEngine().get_user(user_id)), 200
+
+
+@app.route('/user/', methods=['GET'], strict_slashes=False)
+def get_users():
+    return jsonify({'users': DBEngine().get_users()}), 200
+
+
+@app.route('/post/<int:post_id>', methods=['GET'], strict_slashes=False)
+def delete_post(post_id):
+    return jsonify({DBEngine().delete_post(post_id)}), 200
+
+
+@app.route('/user/<int:user_id>', methods=['GET'], strict_slashes=False)
+def delete_user(user_id):
+    return jsonify({DBEngine().delete_user(user_id)}), 200
+
+
+@app.route('/comment/<int:comment_id>', methods=['GET'], strict_slashes=False)
+def delete_comment(comment_id):
+    return jsonify({DBEngine().delete_comment(comment_id)}), 200
+
+
+@app.route('/user/', methods=['POST'], strict_slashes=False)
+def create_user():
+    username = 'stupid'
+    email = 'mike@dumb.com'
+    return jsonify({DBEngine().create_user(username, email)}), 201
+
+
+@app.route('/post/', methods=['POST'], strict_slashes=False)
+def create_post():
+    author = 1
+    title = 'this is a title'
+    content = 'this is some content'
+    return jsonify({DBEngine().create_post(author, title, content)}), 201
+
+
+@app.route('/comment/post/<int:post_id>', methods=['POST'], strict_slashes=False)
+def create_comment():
+    author = 1
+    post_id = 1
+    content = 'this is bullshit'
+    return jsonify({DBEngine().create_user(author, post_id, content)}), 201
+
+
+@app.route('/post/<int:post_id>', methods=['GET'], strict_slashes=False)
+def get_post(post_id):
+    return jsonify({DBEngine().get_post(post_id)}), 200
+
+
+@app.route('/post/', methods=['GET'], strict_slashes=False)
+def get_posts():
+    return jsonify({'posts': DBEngine().get_posts()}), 200
+
+
+@app.route('/comment/<int:comment_id>', methods=['GET'], strict_slashes=False)
+def get_comment(comment_id):
+    return jsonify({DBEngine().get_comment(comment_id)}), 200
+
+
+@app.route('/comment/post/<int:post_id>', methods=['GET'], strict_slashes=False)
+def get_comments(post_id):
+    return jsonify({'comments': DBEngine().get_comments(post_id)}), 200
 
 
 if __name__ == '__main__':
